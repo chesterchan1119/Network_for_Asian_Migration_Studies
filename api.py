0b927c7b512e826fe134fe11257328ca04935f6d
chesterchan1119 <cc11197@gmail.com>
2025-01-26 15:00:49 +0800

    added start.sh

--- api.py
+++ api.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask, jsonify, request, render_template, session, redirect, url_for
+
 app = Flask(__name__, static_folder='../statics', static_url_path='/statics', template_folder='../templates')
 
 app.add_url_rule('/statics/<path:filename>', endpoint='statics', view_func=app.send_static_file)
 
 # Set a random string for the secret key of session
 app.secret_key = "the secret key"
-
 
 @app.route('/', methods=['GET'])
 def index():
